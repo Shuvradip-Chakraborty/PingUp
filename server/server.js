1ee284774ebfd300919c658a8e4f63c3787c3f10
Shuvradip Chakraborty <shuvradipofficial17@gmail.com>
2026-05-09 19:52:17 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/db.js';
+import {inngest, functions} from './Inngest/index.js'
+
+const app = express();
+
+await connectDB();
+
+app.use(express.json());
+app.use(cors())
+
+//creating routes..
+app.get('/', (req, res) => res.send('Server is running')) //home route
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
